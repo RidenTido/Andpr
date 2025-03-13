fd08004a84a59dab7b9abe61b78e3a4d48b03f65
RidenTido <122851974+RidenTido@users.noreply.github.com>
2025-03-13 14:23:05 +0530

    Create Explicit.java

---
+++ Explicit.java
@@ -0,0 +1,68 @@
+package com.explicitIndent;
+
+
+
+import android.app.Activity;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.widget.*;
+
+
+
+public class ExplicitIndentActivity extends Activity {
+
+	
+
+	Button nxtpage;
+
+	EditText textnxt;
+
+	
+
+    @Override
+
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main);
+
+        
+
+        textnxt = (EditText) findViewById(R.id.nxttext);
+
+        nxtpage = (Button) findViewById(R.id.nxtpage);
+
+        
+
+        nxtpage.setOnClickListener(new View.OnClickListener() {
+
+			
+
+			public void onClick(View arg0) {							
+
+				String text = textnxt.getText().toString();
+
+				
+
+				Intent intent = new Intent(ExplicitIndentActivity.this,SecondActivity.class);
+
+				intent.putExtra("Message", text);
+
+				
+
+				startActivity(intent);
+
+				}
+
+		});
+
+        }
+
+}
+
